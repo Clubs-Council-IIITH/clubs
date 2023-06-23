df8201bd0938ee501bf85a952d3fa1c5f2332541
v15hv4 <vishva2912@gmail.com>
2023-06-24 02:14:00 +0530

    Add temporary error handling for db ops

--- db.py
+++ db.py
@@ -17,8 +17,11 @@ client = MongoClient(MONGO_URI)
 # get database
 db = client[MONGO_DATABASE]
 
-db.clubs.create_index([("cid", 1)], unique= True, name="unique_clubs")
-db.members.create_index([("cid", 1),("uid", 1)], unique= True, name="unique_members")
-print(db.clubs.index_information())  
-print(db.members.index_information())
- 
+# TODO: document and properly handle error
+try:
+    db.clubs.create_index([("cid", 1)], unique= True, name="unique_clubs")
+    db.members.create_index([("cid", 1),("uid", 1)], unique= True, name="unique_members")
+    print(db.clubs.index_information())  
+    print(db.members.index_information())
+except:
+    pass
